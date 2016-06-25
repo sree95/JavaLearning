a68f49d72f921212f03bf5dcfbb19d4c6a7bca00
geeksree <sreenivasparimi95@live.in>
2016-06-25 20:27:02 +0530

    Adding Selenium Project

---
+++ SeleniumTraining/src/Day1/Testt1.java
@@ -0,0 +1,14 @@
+package Day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Testt1 {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+
+	}
+
+}
